0bdabecf00aee3f40ebf6943961fe6106be0f666
dengminfeng <1015814408@qq.com>
2021-05-17 23:58:12 +0800

    refeator: 规范化代码结构

--- ivy/center.py
+++ ivy/center.py
@@ -2,6 +2,7 @@
 from ivy.functions.faker import fake
 from ivy.functions import funcs
 from ivy.facade import Facade
+from functools import reduce
 import copy
 
 
@@ -9,6 +10,7 @@ class Center(object):
     context = None
     database = None
     configs = None
+    rules = {}
 
     def __init__(self):
         self.context = Facade().get('context')
@@ -28,18 +30,18 @@ class Center(object):
             }
             self.database.connect(dsn)
 
-            for database in item['databases']:
-                table_name = database.get('table', None)
-                rules = database.get('rules', None)
-                number = database.get('number', 0)
-                chunk = database.get('chunk', 100)
+            for table in item['tables']:
+                table_name = table.get('table', None)
+                rules = table.get('rules', None)
+                number = table.get('number', 0)
+                chunk = table.get('chunk', 100)
 
                 insert_list = []
 
                 while number > 0:
                     if rules is None:
                         break
-                    
+
                     data = self.faker(rules)
                     insert_list.append(data)
                     if len(insert_list) == chunk:
@@ -63,29 +65,33 @@ class Center(object):
             }
 
             self.database.connect(dsn)
-            for database in config['databases']:
-                table_name = database.get('table', None)
-                table_fields = database.get('fields', None)
+            for table in config['tables']:
+                table_name = table.get('table', None)
+                table_fields = table.get('fields', None)
+                flag = table.get('flag', None)
+
+                self.rules[flag] = table.get('rules', None)
+
                 self.database.create_table(table_name, table_fields)
 
     @staticmethod
-    def custom_data_handle(func_, **kwargs):
-        if funcs.get(func_, None) is None:
+    def custom_data_handle(_func, **kwargs):
+        if funcs.get(_func, None) is None:
             raise Exception('func is not define')
         if not kwargs:
-            res = funcs[func_]()
+            res = funcs[_func]()
         else:
-            res = funcs[func_](**kwargs)
+            res = funcs[_func](**kwargs)
         return res
 
     @staticmethod
-    def faker_data_handle(func_, **kwargs):
-        if not hasattr(fake, func_):
+    def faker_data_handle(_func, **kwargs):
+        if not hasattr(fake, _func):
             raise Exception('faker module not have this function')
         if not kwargs:
-            res = getattr(fake, func_)()
+            res = getattr(fake, _func)()
         else:
-            res = getattr(fake, func_)(**kwargs)
+            res = getattr(fake, _func)(**kwargs)
         return res
 
     def faker(self, rules):
@@ -93,11 +99,11 @@ class Center(object):
 
         for k, v in rules.items():
             copy_value = copy.deepcopy(v)
-            func_ = copy_value.pop('func')
-            if 'faker' in func_:
-                insert_value = self.faker_data_handle(func_, **copy_value)
+            _func = copy_value.pop('func')
+            if 'faker' in _func:
+                insert_value = self.faker_data_handle(_func, **copy_value)
             else:
-                insert_value = self.custom_data_handle(func_, **copy_value)
+                insert_value = self.custom_data_handle(_func, **copy_value)
             data[k] = insert_value
 
         return data
--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 #### 概述
+
 ivy是一个配置化的数据填充框架，主要解决的场景
+
 1. 帮助你更好的去处理开发项目前无数据的尴尬
 2. 摆脱手动创建数据库和数据表的繁琐工作
 3. 快速生成数据后台所要的模拟数据，帮助你更快编写数据统计的逻辑开发
@@ -11,15 +13,12 @@ ivy是一个配置化的数据填充框架，主要解决的场景
 代码结构：
 
 ```markdown
-+---config // 批量操作配置文件目录
-+---ivy
-|   +---abstracts // 接口类目录
-|   +---functions // 生成随机数的函数目录
-|   +---manages // 具体逻辑实现管理目录
++---config // 批量操作配置文件目录 +---ivy | +---abstracts // 接口类目录 | +---functions // 生成随机数的函数目录 | +---manages // 具体逻辑实现管理目录
 +---readme // 文档目录
 ```
 
 #### 要求
+
 Python3+
 
 Works on Linux, Windows, Mac OSX, BSD
@@ -27,9 +26,11 @@ Works on Linux, Windows, Mac OSX, BSD
 #### 文档
 
 ##### 自定义填充函数
+
 在 `functions` 目录下编写相关代码即可。
 
 `__init__.py`
+
 ```python
 # -*- coding: utf-8 -*-
 from ivy.functions.date import Date
@@ -50,23 +51,20 @@ funcs = {
 1. 配置实现数据库，数据表自动创建
 2. 填充数据，高度支持faker库
 
-配置文件直接写在 `config` 目录就可以了，不过要注意这里只认识 `.yml` 后缀的配置文件哦。所以只要定义了
-yml的文件都会被执行的哦
+配置文件直接写在 `config` 目录就可以了，不过要注意这里只认识 `.yml` 后缀的配置文件哦。所以只要定义了 yml的文件都会被执行的哦
 
 ###### 栗子：
 
 ```yaml
 
--
-  host: 192.168.3.9
+- host: 192.168.3.9
   port: 9003
   username: root
   password: root
   charset: utf8mb4
   dbname: faker
   databases:
-    -
-      table: faker
+    - table: faker
       fields:
         id: INT NOT NULL AUTO_INCREMENT
         name: VARCHAR(20) NOT NULL
@@ -93,11 +91,9 @@ yml的文件都会被执行的哦
       chunk: 1
 ```
 
-
 ###### tips:
 
-- 这里面使用数组的形式进行配置，每个数组对应一个服务器地址，
-想要填充多个服务器上面的数据库就需要对其进行数组配置
+- 这里面使用数组的形式进行配置，每个数组对应一个服务器地址， 想要填充多个服务器上面的数据库就需要对其进行数组配置
 
 host: 数据库服务器ip
 
@@ -121,7 +117,7 @@ index: 数据表索引
 
 other: 数据表外层engine这些
 
-fill_rule: 数据库填充的内容设置
+rules: 数据库填充的内容设置
 
 number: 要填充的总数量
 
@@ -132,26 +128,26 @@ chunk: 每组批量插入的数量，避免内存溢出 （每次插入数量 = 
 1. 自定义的处理函数要在 `ivy/functions/__init__.py` 中定义函数名称，就是用在配置中的 `func` 哦。
 
 ```yaml
-date: 
-    start: 开始时间
-    end: 结束时间
-    res_format: 生成的时间格式
-    step: 控制时间间隔，具体常量DAY_TO_SECOND，HOUR_TO_SECOND，MINUTE_TO_SECOND,SECOND
+date:
+  start: 开始时间
+  end: 结束时间
+  res_format: 生成的时间格式
+  step: 控制时间间隔，具体常量DAY_TO_SECOND，HOUR_TO_SECOND，MINUTE_TO_SECOND,SECOND
 name:
-    func: default
-    value: test
+  func: default
+  value: test
 ```
 
 2. `faker` 库的调用，这里面要直接带上 `faker` 前缀,然后用|来对其进行分割，这里面不允许使用空格。然后后面带上
-`faker` 库中的函数调用就行了。例如上面例子中，直接使用 `name` 这个函数调用。具体还可以使用什么函数参考 `faker`
-库的官网
+   `faker` 库中的函数调用就行了。例如上面例子中，直接使用 `name` 这个函数调用。具体还可以使用什么函数参考 `faker`
+   库的官网
 
 https://faker.readthedocs.io/en/master/locales/zh_CN.html
-
 
 - 使用
 
 1. 安装需要使用的库
+
 ```shell
 pip install -r requirements.txt
 ```
@@ -159,6 +155,7 @@ pip install -r requirements.txt
 2. 配置padding_data.yml文件，这里按照yaml语法配置就可以了
 
 3. 生成数据库并填充数据
+
 ```shell
 python entry.py
 ```
